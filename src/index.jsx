13ea26438def5a6a6b789494bb6149b153489eeb
Ifeoluwa Gbolahan <ifeoluwag90@gmail.com>
2024-09-01 22:59:52 +0100

    Refractor Pizza component to destructure PizzaObj props for cleaner and effective code

--- src/index.jsx
+++ src/index.jsx
@@ -102,18 +102,18 @@ function Menu() {
 }
 
 //Child component
-function Pizza(props) {
+function Pizza({ pizzaObj }) {
 
-  if(props.pizzaObj.soldOut) return null;
+  if(pizzaObj.soldOut) return null;
 
   return (
     <li className='pizza'>
-      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredients}</p>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
       </div>
-      <span>${props.pizzaObj.price}</span>
+      <span>${pizzaObj.price}</span>
     </li> 
   );
 }
